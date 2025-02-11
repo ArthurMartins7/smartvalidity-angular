5a3d6a76e8330ab3ec661def24366e952d5a4544
ArthurMartins7 <showzin048@gmail.com>
2025-02-11 00:17:15 -0300

    update

---
+++ src/app/components/categoria/categoria-listagem/categoria-listagem.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-categoria-listagem',
+  standalone: true,
+  imports: [],
+  templateUrl: './categoria-listagem.component.html',
+  styleUrl: './categoria-listagem.component.css'
+})
+export class CategoriaListagemComponent {
+
+}

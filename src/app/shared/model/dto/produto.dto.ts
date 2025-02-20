407b4365578bf2ad0d450e5fc7300158c2c9dc93
PietroDaniel <pietrofdaniel@gmail.com>
2025-02-20 14:00:04 -0300

    feat: Criação de categoria.dto.ts, produto.dto.ts, item-produto.dto.ts e rename do arquivo item-produto.seletor.ts

---
+++ src/app/shared/model/dto/produto.dto.ts
@@ -0,0 +1,20 @@
+import { CategoriaDTO } from "./categoria.dto";
+// import { ItemProdutoDTO } from "./item-produto.dto";
+// import { FornecedorDTO } from "./fornecedor.dto";
+// import { AlertaDTO } from "./alerta.dto";
+
+export class ProdutoDTO {
+  id!: number;
+  codigoBarras!: string;
+  descricao!: string;
+  marca!: string;
+  unidadeMedida!: string;
+  quantidade!: number;
+  categoria!: CategoriaDTO;
+
+  // itensProduto!: ItemProdutoDTO[];
+  // fornecedores!: FornecedorDTO[];
+  // alertas!: AlertaDTO[];
+
+}
+

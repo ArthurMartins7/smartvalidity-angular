e2ae9ef8e2b89ab6eca530a7573c811e1baf717e
PietroDaniel <pietrofdaniel@gmail.com>
2025-02-20 13:45:39 -0300

    feat: Criação de base.seletor.ts, categoria.seletor.ts, itemProduto.seletor.ts, produto.seletor.ts

---
+++ src/app/shared/model/seletor/categoria.seletor.ts
@@ -0,0 +1,11 @@
+import { BaseSeletor } from "./base.seletor";
+// import { Corredor } from "./corredor";
+
+export class CategoriaSeletor extends BaseSeletor {
+  nome?: string;
+  produtoId?: number;
+  produtoDescricao?: string;
+
+  // corredorId?: number;
+  // corredorNome?: string;
+}

eb7cd8889d786e894e6e7f4dcbaabf2c5e74e5bd
ARTHUR BOWENS SEQUEIRA <arthur.sequeira@alunos.sc.senac.br>
2025-03-10 08:53:09 -0300

    feat: criação endereco.ts, fornecedor.ts e corredor.ts

---
+++ src/app/shared/model/entity/corredor.ts
@@ -0,0 +1,6 @@
+import { Usuario } from './usuario.model';
+export class Corredor{
+  id: number;
+  nome: string;
+  responsaveis: Usuario[];
+}
---
+++ src/app/shared/model/entity/fornecedor.ts
@@ -0,0 +1,10 @@
+import { Endereco } from "./endereco";
+
+export class Fornecedor{
+
+  id: number;
+  nome: string;
+  telefone: string;
+  cnpj: string;
+  endereco: Endereco;
+}

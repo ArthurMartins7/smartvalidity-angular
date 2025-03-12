2c49b072ad83f995a53d99ea98db6af5af547907
Arthur Bowens Sequeira <arthur.sequeira@pmsj.sc.gov.br>
2025-03-12 18:26:21 -0300

    update

---
+++ src/app/components/fornecedor/fornecedor-listagem/fornecedor-listagem.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fornecedor-listagem',
+  imports: [],
+  templateUrl: './fornecedor-listagem.component.html',
+  styleUrl: './fornecedor-listagem.component.css'
+})
+export class FornecedorListagemComponent {
+
+}
--- src/app/shared/model/dto/corredor.dto.ts
+++ src/app/shared/model/dto/corredor.dto.ts
@@ -1,5 +1,7 @@
+import { Usuario } from "../entity/usuario.model";
+
 export class CorredorDTO{
   id: number;
   nome: string;
-  //responsaveis: Usuario[];
+  responsaveis: Usuario[];
 }

d9dc8a18389635d34319e14815a988cb18768e0d
ArthurMartins7 <showzin048@gmail.com>
2025-03-27 21:59:32 -0300

    update

--- src/app/core/auth/pages/register/register/register.component.ts
+++ src/app/core/auth/pages/register/register/register.component.ts
@@ -37,7 +37,7 @@ export class RegisterComponent implements OnInit{
         this.usuarios = resultado;
       },
       (erro) => {
-        console.error('Erro ao consultar todos os usuários', erro.error);
+        console.error('Erro ao consultar todos os usuário', erro.error);
       }
       );
   }

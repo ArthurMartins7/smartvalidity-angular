c0865f645d806922771761825f413a643515202b
ARTHUR HENRIQUE SILVA MARTINS <arthur.martins@alunos.sc.senac.br>
2025-03-31 11:04:18 -0300

    feat: sidebar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { LoginComponent } from './core/auth/pages/login/login/login.component';
 import { RegisterComponent } from './core/auth/pages/register/register/register.component';
 import { CorredorListagemComponent } from './components/corredor/corredor-listagem/corredor-listagem.component';
 import { CorredorDetalheComponent } from './components/corredor/corredor-detalhe/corredor-detalhe.component';
+import { ProdutoListagemComponent } from './components/produto/produto-listagem/produto-listagem.component';
 
 export const routes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'corredor', component: CorredorListagemComponent },
-  { path: 'corredor-detalhe', component: CorredorDetalheComponent }
+  { path: 'corredor-detalhe', component: CorredorDetalheComponent },
+  { path: 'produto-listagem', component: ProdutoListagemComponent },
 
 ];

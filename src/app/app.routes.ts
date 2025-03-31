af5e1843b1634d8a689f5ecf15f951ee6a364c1d
PietroDaniel <pietrofdaniel@gmail.com>
2025-03-31 10:21:10 -0300

    Update app.routes.ts: mural paths

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,15 @@ import { LoginComponent } from './core/auth/pages/login/login/login.component';
 import { RegisterComponent } from './core/auth/pages/register/register/register.component';
 import { CorredorListagemComponent } from './components/corredor/corredor-listagem/corredor-listagem.component';
 import { CorredorDetalheComponent } from './components/corredor/corredor-detalhe/corredor-detalhe.component';
+import { MuralListagemComponent } from './components/mural/mural-listagem/mural-listagem.component';
+import { MuralDetalheComponent } from './components/mural/mural-detalhe/mural-detalhe.component';
+
 
 export const routes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'corredor', component: CorredorListagemComponent },
-  { path: 'corredor-detalhe', component: CorredorDetalheComponent }
-
+  { path: 'corredor-detalhe', component: CorredorDetalheComponent },
+  { path: 'mural-listagem', component: MuralListagemComponent }
+  { path: 'mural-detalhe', component: MuralDetalheComponent },
 ];

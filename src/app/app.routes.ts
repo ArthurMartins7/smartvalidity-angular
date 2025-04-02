2e3c82c45098edad33d2a8282b084208b2c6b479
PietroDaniel <pietrofdaniel@gmail.com>
2025-04-02 09:58:37 -0300

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,8 @@ import { RegisterComponent } from './core/auth/pages/register/register/register.
 import { CorredorListagemComponent } from './components/corredor/corredor-listagem/corredor-listagem.component';
 import { CorredorDetalheComponent } from './components/corredor/corredor-detalhe/corredor-detalhe.component';
 import { ProdutoListagemComponent } from './components/produto/produto-listagem/produto-listagem.component';
+import { MuralListagemComponent } from './components/mural/mural-listagem/mural-listagem.component';
+import { MuralDetalheComponent } from './components/mural/mural-detalhe/mural-detalhe.component';
 
 export const routes: Routes = [
   // Auth routes (without sidebar)
@@ -17,7 +19,7 @@ export const routes: Routes = [
   { path: '', redirectTo: 'produto-listagem', pathMatch: 'full' },
 
   // Catch all route
-  { path: '**', redirectTo: 'login' }
+  { path: '**', redirectTo: 'login' },
   { path: 'corredor-detalhe', component: CorredorDetalheComponent },
   { path: 'mural-listagem', component: MuralListagemComponent },
   { path: 'mural-detalhe', component: MuralDetalheComponent }

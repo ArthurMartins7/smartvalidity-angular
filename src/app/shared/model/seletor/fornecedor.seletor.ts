9b6a0f9a6f43f157255f0d840ec33ffdcf650090
Arthur Bowens Sequeira <arthur.sequeira@pmsj.sc.gov.br>
2025-04-08 18:39:07 -0300

    update

--- src/app/shared/model/seletor/fornecedor.seletor.ts
+++ src/app/shared/model/seletor/fornecedor.seletor.ts
@@ -1,9 +1,16 @@
-import { Produto } from "../entity/produto";
 import { BaseSeletor } from "./base.seletor";
 
 export class FornecedorSeletor extends BaseSeletor {
   nome: string;
   telefone: string;
   cnpj: string;
-  produto: Produto[];
+  descricaoProduto: string;
+  logradouro: string;
+  numero: string;
+  complemento: string;
+  bairro: string;
+  cidade: string;
+  estado: string;
+  pais: string;
+  cep: string;
 }

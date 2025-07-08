556f5e27aa4f43f0b8d92fb7e4c5f1b514f6a6dd
PietroDaniel <139817787+PietroDaniel@users.noreply.github.com>
2025-07-07 22:25:20 -0300

    update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://31.97.251.133:8080/smartvalidity'
+  //apiUrl: 'http://31.97.251.133:8080/smartvalidity'
+  apiUrl: 'https://validata.shop'
 };
